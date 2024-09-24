a211e228b0d701b53c7110c95c363596022d3397
xu-wang11 <wangxu.93@icloud.com>
2024-09-24 10:46:56 +0800

    add icdcs conference

--- networking-deadlines/_site/static/js/main.js
+++ networking-deadlines/_site/static/js/main.js
@@ -249,6 +249,69 @@ $(function() {
   
   
   
+  // ICDCS 2025
+  
+  var rawDeadlines = ["2024-12-11 23:59:59"] || [];
+  if (rawDeadlines.constructor !== Array) {
+    rawDeadlines = [rawDeadlines];
+  }
+  var parsedDeadlines = [];
+  while (rawDeadlines.length > 0) {
+    var rawDeadline = rawDeadlines.pop();
+    // deal with year template in deadline
+    year = 2025;
+    rawDeadline = rawDeadline.replace('%y', year).replace('%Y', year - 1);
+    // adjust date according to deadline timezone
+    
+    var deadline = moment.tz(rawDeadline, "Etc/GMT+12"); // Anywhere on Earth
+    
+
+    // post-process date
+    if (deadline.minutes() === 0) {
+      deadline.subtract(1, 'seconds');
+    }
+    if (deadline.minutes() === 59) {
+      deadline.seconds(59);
+    }
+    parsedDeadlines.push(deadline);
+  }
+  // due to pop before; we need to reverse such that the i index later matches
+  // the right parsed deadline
+  parsedDeadlines.reverse();
+
+  
+  
+  
+  var deadlineId = 0;
+  if (deadlineId < parsedDeadlines.length) {
+    var confDeadline = parsedDeadlines[deadlineId];
+
+    // render countdown timer
+    if (confDeadline) {
+      function make_update_countdown_fn(confDeadline) {
+        return function(event) {
+          diff = moment() - confDeadline
+          if (diff <= 0) {
+             $(this).html(event.strftime('%D days %Hh %Mm %Ss'));
+          } else {
+            $(this).html(confDeadline.fromNow());
+          }
+        }
+      }
+      $('#icdcs2025-0 .timer').countdown(confDeadline.toDate(), make_update_countdown_fn(confDeadline));
+      // check if date has passed, add 'past' class to it
+      if (moment() - confDeadline > 0) {
+        $('#icdcs2025-0').addClass('past');
+      }
+      $('#icdcs2025-0 .deadline-time').html(confDeadline.local().format('D MMM YYYY, h:mm:ss a'));
+      deadlineByConf["icdcs2025-0"] = confDeadline;
+    }
+  } else {
+    // TODO: hide the conf_id ?
+  }
+  
+  
+  
   // SIGCOMM 2024
   
   var rawDeadlines = ["2024-02-02 23:59"] || [];
